ba5fab8428d6e6810b3e5a26319f7a35bfc9ad61
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-10-19 09:59:06 -0400

    add license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Copyright 2023 The Regents of the University of California
+// released under BSD 3-Clause License
+// author: Kevin Laeufer <laeufer@berkeley.edu>
+
 fn main() {
     println!("Hello, world!");
 }
