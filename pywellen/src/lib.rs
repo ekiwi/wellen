641059046a5bfa1818e34732067e50467b03bc63
Roman Popov <ripopov@gmail.com>
2025-07-14 20:25:53 -0700

    pywellen : add essential Var, Scope and Waveform metadata accessors
    Added python bindings for essential APIs needed to build waveform viewier in Python:
       Var types and directions, scope types, timescale.
    
    Added VCD file with GtkWave scope extensions (generated by GtkWave).

--- pywellen/src/lib.rs
+++ pywellen/src/lib.rs
@@ -8,7 +8,7 @@ use pyo3::{exceptions::PyRuntimeError, prelude::*};
 
 use wellen::{
     viewers::{self},
-    LoadOptions, SignalValue, TimeTableIdx,
+    LoadOptions, SignalValue, TimeTableIdx, ScopeType,
 };
 
 pub trait PyErrExt<T> {
@@ -28,6 +28,9 @@ fn pywellen(_py: Python, m: Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Waveform>()?;
     m.add_class::<Signal>()?;
     m.add_class::<SignalChangeIter>()?;
+    m.add_class::<Hierarchy>()?;
+    m.add_class::<Timescale>()?;
+    m.add_class::<TimescaleUnit>()?;
     Ok(())
 }
 
@@ -57,6 +60,31 @@ impl Hierarchy {
                 .into_iter()
         }))
     }
+
+    /// Get the date metadata from the waveform file
+    fn date(&self) -> String {
+        self.0.date().to_string()
+    }
+
+    /// Get the version metadata from the waveform file
+    fn version(&self) -> String {
+        self.0.version().to_string()
+    }
+
+    /// Get the timescale metadata from the waveform file
+    fn timescale(&self) -> Option<Timescale> {
+        self.0.timescale().map(Timescale)
+    }
+
+    /// Get the file format of the waveform file
+    fn file_format(&self) -> String {
+        match self.0.file_format() {
+            wellen::FileFormat::Vcd => "VCD".to_string(),
+            wellen::FileFormat::Fst => "FST".to_string(),
+            wellen::FileFormat::Ghw => "GHW".to_string(),
+            wellen::FileFormat::Unknown => "Unknown".to_string(),
+        }
+    }
 }
 
 #[pyclass]
@@ -69,6 +97,37 @@ impl Scope {
     }
     pub fn full_name(&self, hier: Bound<'_, Hierarchy>) -> String {
         self.0.full_name(&hier.borrow().0).to_string()
+    }
+
+    pub fn scope_type(&self) -> String {
+        match self.0.scope_type() {
+            ScopeType::Module => "module",
+            ScopeType::Task => "task",
+            ScopeType::Function => "function",
+            ScopeType::Begin => "begin",
+            ScopeType::Fork => "fork",
+            ScopeType::Generate => "generate",
+            ScopeType::Struct => "struct",
+            ScopeType::Union => "union",
+            ScopeType::Class => "class",
+            ScopeType::Interface => "interface",
+            ScopeType::Package => "package",
+            ScopeType::Program => "program",
+            ScopeType::VhdlArchitecture => "vhdl_architecture",
+            ScopeType::VhdlProcedure => "vhdl_procedure",
+            ScopeType::VhdlFunction => "vhdl_function",
+            ScopeType::VhdlRecord => "vhdl_record",
+            ScopeType::VhdlProcess => "vhdl_process",
+            ScopeType::VhdlBlock => "vhdl_block",
+            ScopeType::VhdlForGenerate => "vhdl_for_generate",
+            ScopeType::VhdlIfGenerate => "vhdl_if_generate",
+            ScopeType::VhdlGenerate => "vhdl_generate",
+            ScopeType::VhdlPackage => "vhdl_package",
+            ScopeType::GhwGeneric => "ghw_generic",
+            ScopeType::VhdlArray => "vhdl_array",
+            ScopeType::Unknown => "unknown",
+            _ => "unknown", // `ScopeType` is marked as non-exhaustive
+        }.to_string()
     }
 
     pub fn vars(&self, hier: Bound<'_, Hierarchy>) -> VarIter {
@@ -134,6 +193,35 @@ impl Var {
     pub fn bitwidth(&self) -> Option<u32> {
         self.0.length()
     }
+    pub fn var_type(&self) -> String {
+        format!("{:?}", self.0.var_type())
+    }
+    pub fn enum_type(&self, hier: Bound<'_, Hierarchy>) -> Option<(String, Vec<(String, String)>)> {
+        self.0.enum_type(&hier.borrow().0).map(|(name, values)| {
+            (name.to_string(), values.into_iter().map(|(k, v)| (k.to_string(), v.to_string())).collect())
+        })
+    }
+    pub fn vhdl_type_name(&self, hier: Bound<'_, Hierarchy>) -> Option<String> {
+        self.0.vhdl_type_name(&hier.borrow().0).map(|s| s.to_string())
+    }
+    pub fn direction(&self) -> String {
+        format!("{:?}", self.0.direction())
+    }
+    pub fn length(&self) -> Option<u32> {
+        self.0.length()
+    }
+    pub fn is_real(&self) -> bool {
+        self.0.is_real()
+    }
+    pub fn is_string(&self) -> bool {
+        self.0.is_string()
+    }
+    pub fn is_bit_vector(&self) -> bool {
+        self.0.is_bit_vector()
+    }
+    pub fn is_1bit(&self) -> bool {
+        self.0.is_1bit()
+    }
 }
 
 #[pyclass]
@@ -146,6 +234,58 @@ impl VarIter {
     }
     fn __next__(mut slf: PyRefMut<'_, Self>) -> Option<Var> {
         slf.0.next()
+    }
+}
+
+#[pyclass(name = "TimescaleUnit")]
+#[derive(Clone)]
+struct TimescaleUnit(pub(crate) wellen::TimescaleUnit);
+
+#[pymethods]
+impl TimescaleUnit {
+    fn __str__(&self) -> String {
+        match self.0 {
+            wellen::TimescaleUnit::FemtoSeconds => "fs".to_string(),
+            wellen::TimescaleUnit::PicoSeconds => "ps".to_string(),
+            wellen::TimescaleUnit::NanoSeconds => "ns".to_string(),
+            wellen::TimescaleUnit::MicroSeconds => "us".to_string(),
+            wellen::TimescaleUnit::MilliSeconds => "ms".to_string(),
+            wellen::TimescaleUnit::Seconds => "s".to_string(),
+            wellen::TimescaleUnit::Unknown => "unknown".to_string(),
+        }
+    }
+
+    fn __repr__(&self) -> String {
+        format!("TimescaleUnit.{}", self.__str__())
+    }
+
+    fn to_exponent(&self) -> Option<i8> {
+        self.0.to_exponent()
+    }
+}
+
+#[pyclass(name = "Timescale")]
+#[derive(Clone)]
+struct Timescale(pub(crate) wellen::Timescale);
+
+#[pymethods]
+impl Timescale {
+    #[getter]
+    fn factor(&self) -> u32 {
+        self.0.factor
+    }
+
+    #[getter]
+    fn unit(&self) -> TimescaleUnit {
+        TimescaleUnit(self.0.unit)
+    }
+
+    fn __str__(&self) -> String {
+        format!("{}{}", self.0.factor, TimescaleUnit(self.0.unit).__str__())
+    }
+
+    fn __repr__(&self) -> String {
+        format!("Timescale(factor={}, unit={})", self.0.factor, TimescaleUnit(self.0.unit).__repr__())
     }
 }
 
